29fc36811a9df9c92fe40c8575d5697ac9447af8
t6767@mail.ru <t6767@mail.ru>
2019-11-25 11:23:40 +0600

    up

--- footer.php
+++ footer.php
@@ -140,6 +140,7 @@
 <!--/. Modals End -->
 
 <!-- Main scripts. You can replace it, but I recommend you to leave it here -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/static/js/main.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/static/js/separate-js/scripts.js"></script>
 </div>
