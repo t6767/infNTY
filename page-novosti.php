558b8824ac18bbba32a09ed5c165bd332ade66f2
t6767@mail.ru <t6767@mail.ru>
2019-12-11 16:38:31 +0600

    гз

--- page-novosti.php
+++ page-novosti.php
@@ -6,13 +6,6 @@
     <div class="page">
 
         <!-- Page Header Begin -->
-        <?php
-        $nflag=1;
-        if ($_SERVER[REQUEST_URI]=="/ru/novosti/")
-        {
-            $nflag=0;
-        }
-        ?>
         <div class="page__header" style="background-image: url('<?php bloginfo('template_url'); ?>/static/img/content/header-bg.jpg')">
             <div class="container">
                 <div class="page__header-title typed-effect">
@@ -22,133 +15,131 @@
                 </div>
                 <ul class="breadcrumbs">
                     <li class="breadcrumbs__item">
-                        <a href="/" class="breadcrumbs__link">Главная</a>
+                        <a href="#" class="breadcrumbs__link">Главная</a>
                     </li>
-                        <li class="breadcrumbs__item">
-                            Новости
-                        </li>
+                    <li class="breadcrumbs__item">
+                        <?php the_title(); ?>
+                    </li>
                 </ul>
             </div>
         </div>
         <!--/. Page Header End -->
+
         <!-- Page Content Begin -->
         <div class="page__content">
             <div class="container">
-                <section>
-                    <div class="page__topbar">
-                        <div class="category-select dropdown">
-                            <a href="#" class="category-select__link" id="novblock" data-toggle="dropdown">Новости</a>
-                            <div class="dropdown-menu category-select__menu">
-                                <a href="javascript:;" onclick="getNews(1); location.reload();" class="category-select__sublink">Все новости</a>
-                                <a href="javascript:;" onclick="getNews(2);" class="category-select__sublink">События</a>
-                                <a href="javascript:;" onclick="getNews(3);" class="category-select__sublink">Мероприятия</a>
-                                <a href="javascript:;" onclick="getNews(4);" class="category-select__sublink">СМИ о нас</a>
-                            </div>
-                        </div>
-                        <button type="button" class="button -bordered" data-target="#modal-cta" data-toggle="modal">контакт с pr-службой</button>
+                <div class="page__topbar">
+                    <ul class="nav tabs">
+                        <li class="tabs__item">
+                            <a class="tabs__link active" data-toggle="tab" href="#tabs-1" role="tab">Новости</a>
+                        </li>
+                        <li class="tabs__item">
+                            <a class="tabs__link" data-toggle="tab" href="#tabs-2" role="tab">События</a>
+                        </li>
+                        <li class="tabs__item">
+                            <a class="tabs__link" data-toggle="tab" href="#tabs-3" role="tab">Мероприятия</a>
+                        </li>
+                        <li class="tabs__item">
+                            <a class="tabs__link" data-toggle="tab" href="#tabs-4" role="tab">СМИ о нас</a>
+                        </li>
+                    </ul>
+                    <!--<div class="category-select dropdown">
+                    <a href="#" class="category-select__link" data-toggle="dropdown">Новости</a>
+                    <div class="dropdown-menu category-select__menu">
+                        <a href="#" class="category-select__sublink">Все категории</a>
+                        <a href="#" class="category-select__sublink">Корпоративный сектор</a>
+                        <a href="#" class="category-select__sublink">Государственный сектор</a>
                     </div>
-                    <div class="row" id="ajax" vertical-gutter="40" data-gutter="40">
+                </div>-->
+                    <button type="button" class="button -bordered page__topbar-button" data-target="#modal-cta" data-toggle="modal">контакт с pr-службой</button>
+                </div>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="tabs-1" role="tabpanel">
+
+                        <div class="row" vertical-gutter="40" data-gutter="40">
+                            <?php
+                            global $paged;
+                            if ( get_query_var('paged') )
+                                $my_page = get_query_var('paged');
+                            else {
+                                if ( get_query_var('page') )
+                                    $my_page = get_query_var('page');
+                                else
+                                    $my_page = 1;
+                                set_query_var('paged', $my_page);
+                                $paged = $my_page;
+                            }
 
 
-
-                        <?php
-                        global $paged;
-                        if ( get_query_var('paged') )
-                            $my_page = get_query_var('paged');
-                        else {
-                            if ( get_query_var('page') )
-                                $my_page = get_query_var('page');
-                            else
-                                $my_page = 1;
-                            set_query_var('paged', $my_page);
-                            $paged = $my_page;
-                        }
-
-
-                        $query = new WP_Query(
-                            array(
-                                'post_type' => 'post',
-                                'post_status' => 'publish',
-                                'posts_per_page' => 4,
-                                'cat' => [17,19,18,7],  //1 7 8
-                                'post__not_in' => array(get_the_ID()),
-                                'orderby' => 'date',
-                                'order' => 'DESC',
-                                'paged' => $my_page
-                            )
-                        );
+                            $query = new WP_Query(
+                                array(
+                                    'post_type' => 'post',
+                                    'post_status' => 'publish',
+                                    'posts_per_page' => 4,
+                                    'cat' => [17,19,18,7],  //1 7 8
+                                    'post__not_in' => array(get_the_ID()),
+                                    'orderby' => 'date',
+                                    'order' => 'DESC',
+                                    'paged' => $my_page
+                                )
+                            );
 
 
-                        if ($query->have_posts()) {
-                            while ($query->have_posts()) {
-                                $query->the_post();
-                                $date = get_the_date( "d.m.Y");
-                                ?>
+                            if ($query->have_posts()) {
+                                while ($query->have_posts()) {
+                                    $query->the_post();
+                                    $date = get_the_date( "d.m.Y");
+                                    ?>
 
-                                <div class="col-md-6">
-                                    <div class="news">
-                                        <a href="<?php the_permalink(); ?>">
-                                            <?php the_post_thumbnail(array(533 ,200)); ?>
-                                        </a>
-                                        <div class="news__content">
-                                            <a href="<?php the_permalink(); ?>" class="news__date"><?=$date ?></a> / <?php the_category(', '); ?>
-                                            <a href="<?php the_permalink(); ?>" class="news__title"><?php the_title(); ?></a>
-                                            <div class="news__text">
-                                                <?=new_excerpt_more(the_excerpt_max_charlength(150))?>
+                                    <div class="col-md-6">
+                                        <div class="news">
+                                            <a href="<?php the_permalink(); ?>">
+                                                <?php the_post_thumbnail(array(533 ,200)); ?>
+                                            </a>
+                                            <div class="news__content">
+                                                <a href="<?php the_permalink(); ?>" class="news__date"><?=$date ?></a> / <?php the_category(', '); ?>
+                                                <a href="<?php the_permalink(); ?>" class="news__title"><?php the_title(); ?></a>
+                                                <div class="news__text">
+                                                    <?=new_excerpt_more(the_excerpt_max_charlength(150))?>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            <?php
+                                    <?php
+                                }
+                                if(function_exists('wp_pagenavi')) {
+                                    ?>
+                                    <div class="col-md-12">
+                                        <?php
+                                        wp_pagenavi(array('query' => $query));
+                                        ?>
+                                    </div>
+                                    <?php
+                                    $wp_query = null;
+                                    $wp_query = $query;
+                                }
+                                wp_reset_postdata();
                             }
-                            if(function_exists('wp_pagenavi')) {
-                             ?>
-                                <div class="col-md-12">
-                                <?php
-                                wp_pagenavi(array('query' => $query));
-                                ?>
-                                </div>
-                                <?php
-                                $wp_query = null;
-                                $wp_query = $query;
-                            }
-                            wp_reset_postdata();
-                        }
-                        ?>
-
+                            ?>
+                        </div>
+                    </div>
+                    <div class="tab-pane" id="tabs-2" role="tabpanel">
 
                     </div>
-                    <script>
-                        function getNews(operation) {
-                            switch (operation) {
-                                case 1: document.getElementById('novblock').innerText='Новости'; break;
-                                case 2: document.getElementById('novblock').innerText='События'; break;
-                                case 3: document.getElementById('novblock').innerText='Мероприятия'; break;
-                                case 4: document.getElementById('novblock').innerText='СМИ о нас'; break;
-                            }
+                    <div class="tab-pane" id="tabs-3" role="tabpanel">
 
-                            $.ajax({
-                                type:'POST',
-                                url:'/ajax.php',
-                                data:{
-                                    'operation':operation
-                                },
-                                success:function(html){
-                                    $('#ajax').html(html);
-                                },
-                                error:function(html){
-                                    $('body').css('cursor','default');
-                                    alert('Ошибка подключения!');
-                                },
-                            });
-                        }
-                    </script>
-                </section>
+                    </div>
+                    <div class="tab-pane" id="tabs-4" role="tabpanel">
+
+                    </div>
+                </div>
             </div>
         </div>
         <!--/. Page Content End -->
+
     </div>
     <!--/. Page End -->
+
 </main>
 <!--/. App Main End -->
 <?php get_footer(); ?>
