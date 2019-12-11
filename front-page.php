08bef86804221a676f92917015e6208a856d6c8f
t6767@mail.ru <t6767@mail.ru>
2019-12-11 16:24:52 +0600

    гз

--- front-page.php
+++ front-page.php
@@ -167,7 +167,7 @@
                 <div class="container">
                     <div class="row" vertical-gutter="30">
                         <div class="col-md-4">
-                            <a href="/o-kompanii/" class="info-card">
+                            <a href="/about/" class="info-card">
                                 <div class="info-card__title">О Компании</div>
                                 <div class="info-card__text">InFin-IT Solution является научно-производственной компанией, занимающейся разработкой инновационных решений в области цифровых каналов для международного рынка.</div>
                                 <div class="info-card__link">Подробнее</div>
