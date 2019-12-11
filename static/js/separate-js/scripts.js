a524e7c94ba881365abc6614cd719071862946c0
t6767@mail.ru <t6767@mail.ru>
2019-12-11 10:25:31 +0600

    up

--- static/js/separate-js/scripts.js
+++ static/js/separate-js/scripts.js
@@ -8,21 +8,7 @@ $(function() {
     |--------------------------------------------------------------------------
     */
 
-    var wow = new WOW(
-        {
-            boxClass:     'wow',      // animated element css class (default is wow)
-            animateClass: 'animated', // animation css class (default is animated)
-            offset:       0,          // distance to the element when triggering the animation (default is 0)
-            mobile:       true,       // trigger animations on mobile devices (default is true)
-            live:         true,       // act on asynchronously loaded content (default is true)
-            callback:     function(box) {
-                // the callback is fired every time an animation is started
-                // the argument that is passed in is the DOM node being animated
-            },
-            scrollContainer: null,    // optional scroll container selector, otherwise use window,
-            resetAnimation: true,     // reset animation on end (default is true)
-        }
-    );
+    var wow = new WOW();
     wow.init();
 
     /*
@@ -35,7 +21,7 @@ $(function() {
         $(this).attr("data-count", $(this).text());
     });
 
-    $(".intro-about").on("wow", function(){
+    $(".intro-about__stats").on("wow", function(){
         $('.jsCountStats').each(function (){
             $(this).stop().prop('Counter',0).animate({
                 Counter: $(this).attr("data-count")
@@ -316,7 +302,7 @@ $(function() {
     */
 
 	$('.lg').lightGallery({
-		selector: ".lg__item",
+		//selector: ".lg__item",
 	});
 
     /*
