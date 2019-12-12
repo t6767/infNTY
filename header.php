8660f6f6eb34cf0cf55c6cee80de602a1ada6228
t6767@mail.ru <t6767@mail.ru>
2019-12-12 16:48:44 +0600

    ty

--- header.php
+++ header.php
@@ -72,6 +72,10 @@ function the_excerpt_max_charlength( $charlength ){
             color: #1a73e8;
             text-decoration: none;
         }
+        .footer__link
+        {
+            color: #000;
+        }
     </style>
     <?php wp_head(); ?>
 </head>
