b8e139a0fcf5823bb7c15d31e62386df73a392a5
t6767@mail.ru <t6767@mail.ru>
2019-12-12 18:01:15 +0600

    ds

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php
 $post = $wp_query->post;
 
-if (in_category('vakansii')) { //slug  категории
+if (in_category('20')) { //ID категории
     include(TEMPLATEPATH.'/single-portfolio.php');
 } else {
     include(TEMPLATEPATH.'/single-default.php');
