98e9c020c0ce4c8a603de8411413849f167d44b5
t6767@mail.ru <t6767@mail.ru>
2019-12-13 10:41:22 +0600

    up

--- single-default.php
+++ single-default.php
@@ -1,12 +1,46 @@
-<?php get_header(); ?>
-<div class="container">
-    <h1><?=$wp_query->post->post_title ?></h1>
-    <?php
-echo $wp_query->post->post_content;
-echo "<pre>";
-var_dump($wp_query->post);
-echo "</pre>";
-?>
-</div>
-<?php the_post_thumbnail(array(533 ,200)); ?>
+<main role="main" class="app__main">
+
+    <!-- Page Begin -->
+    <div class="page">
+
+        <!-- Page Header Begin -->
+        <div class="page__header" style="background-image: url('static/img/content/header-bg.jpg')">
+            <div class="container">
+                <div class="page__header-title typed-effect">
+                    <div id="js-typed-strings">
+                        <p><?=$wp_query->post->post_title ?></p>
+                    </div>
+                </div>
+                <ul class="breadcrumbs">
+                    <li class="breadcrumbs__item">
+                        <a href="/" class="breadcrumbs__link">Главная</a>
+                    </li>
+                    <li class="breadcrumbs__item">
+                        <?=$wp_query->post->post_title ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!--/. Page Header End -->
+
+        <!-- Page Content Begin -->
+        <div class="page__content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="article">
+                            <div style="float: right"><?php the_post_thumbnail(); ?> </div>
+                            <?php echo $wp_query->post->post_content; ?>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <!--/. Page Content End -->
+
+    </div>
+    <!--/. Page End -->
+
+</main>
 <?php get_footer(); ?>
