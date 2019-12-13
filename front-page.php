ab2caea9ff0e39d3e75c5bdc9c3e43cc008abd2d
t6767@mail.ru <t6767@mail.ru>
2019-12-13 15:05:29 +0600

    up

--- front-page.php
+++ front-page.php
@@ -216,10 +216,12 @@
                                 if ($i==0) {echo '<div class="features__col">';}
                                 $query->the_post();
                                 $dpole = get_post_meta( get_the_ID(), 'vid' );
+                                $icon = get_post_meta( get_the_ID(), 'icon' );
                                 if ((int)$dpole[0] > 0) $style=" -inverted"; else $style="";
+                                if ($icon[0]!='') $ic=$icon[0]; else $ic="features-icon-1.svg";
                                 ?>
                                 <a href="<?=get_permalink()?>" class="features__item<?=$style?>">
-                                    <img src="<?php bloginfo('template_url'); ?>/static/img/general/features-icon-1.svg" class="features__icon" alt="">
+                                    <img src="<?php bloginfo('template_url'); ?>/static/img/general/<?=$ic?>>" class="features__icon" alt="">
                                     <div class="features__title"><?php the_title(); ?></div>
                                     <div class="features__text"><?=the_excerpt_max_charlength(150)?></div>
                                     <div class="features__link">Подробнее</div>
