762eaf2db27b4dd3d83527ce838baddd5520e5de
t6767@mail.ru <t6767@mail.ru>
2019-12-13 17:01:40 +0600

    up

--- search.php
+++ search.php
@@ -1,33 +1,73 @@
-<?php
-get_header(); ?>
+<?php get_header(); ?>
+<!-- App Main Begin -->
+<main role="main" class="app__main">
 
-    <section id="primary" class="content-area">
-        <div id="content" class="site-content" role="main">
+    <!-- Page Begin -->
+    <div class="page">
 
-            <?php if ( have_posts() ) : ?>
+        <!-- Page Header Begin -->
+        <div class="page__header" style="background-image: url('static/img/content/header-bg.jpg')">
+            <div class="container">
+                <div class="page__header-title typed-effect">
+                    <div id="js-typed-strings">
+                        <p>Поиск по сайту</p>
+                    </div>
+                </div>
+                <ul class="breadcrumbs">
+                    <li class="breadcrumbs__item">
+                        <a href="#" class="breadcrumbs__link">Поиск по сайту</a>
+                    </li>
+                    <li class="breadcrumbs__item">
+                        Поиск по сайту
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!--/. Page Header End -->
 
-                <header class="page-header">
-                    <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'shape' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-                </header><!-- .page-header -->
+        <!-- Page Content Begin -->
+        <div class="page__content">
+            <div class="container">
+                <div class="search-results">
+                    <form class="search-results__header">
+                        <div class="control mb-0">
+                            <input type="search" class="control__input search-results__input" placeholder="Найти">
+                        </div>
+                        <button type="submit" class="button -bordered search-results__button">Искать</button>
+                    </form>
+                    <h5>Результаты поиска 23</h5>
+                    <div class="search-results__item">
+                        <a href="#" class="search-results__title">Расчет стоимости ИТ-сервисов</a>
+                        <p>... ИТ сервисов, определенное на уровне правления или высшего руководства. Названия таких структур попадают в одну из следующих категорий: Разработка прикладных систем Эксплуатация Поддержка Безопасность Архитектурное
+                            планирование И так далее Эти структуры начинают ссылаться ...</p>
+                        <p class="mb-2">Дата: 23.05.2019</p>
+                        <p>Путь: <a href="#">Главная</a> / <a href="#">Новости</a>
+                        </p>
+                    </div>
+                    <div class="search-results__item">
+                        <a href="#" class="search-results__title">Расчет стоимости ИТ-сервисов</a>
+                        <p>... ИТ сервисов, определенное на уровне правления или высшего руководства. Названия таких структур попадают в одну из следующих категорий: Разработка прикладных систем Эксплуатация Поддержка Безопасность Архитектурное
+                            планирование И так далее Эти структуры начинают ссылаться ...</p>
+                        <p class="mb-2">Дата: 23.05.2019</p>
+                        <p>Путь: <a href="#">Главная</a> / <a href="#">Новости</a>
+                        </p>
+                    </div>
+                    <div class="search-results__item">
+                        <a href="#" class="search-results__title">Расчет стоимости ИТ-сервисов</a>
+                        <p>... ИТ сервисов, определенное на уровне правления или высшего руководства. Названия таких структур попадают в одну из следующих категорий: Разработка прикладных систем Эксплуатация Поддержка Безопасность Архитектурное
+                            планирование И так далее Эти структуры начинают ссылаться ...</p>
+                        <p class="mb-2">Дата: 23.05.2019</p>
+                        <p>Путь: <a href="#">Главная</a> / <a href="#">Новости</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/. Page Content End -->
 
-                <?php shape_content_nav( 'nav-above' ); ?>
+    </div>
+    <!--/. Page End -->
 
-                <?php /* Start the Loop */ ?>
-                <?php while ( have_posts() ) : the_post(); ?>
-
-                    <?php get_template_part( 'content', 'search' ); ?>
-
-                <?php endwhile; ?>
-
-                <?php shape_content_nav( 'nav-below' ); ?>
-
-            <?php else : ?>
-
-                <?php get_template_part( 'no-results', 'search' ); ?>
-
-            <?php endif; ?>
-
-        </div><!-- #content .site-content -->
-    </section><!-- #primary .content-area -->
-
+</main>
+<!--/. App Main End -->
 <?php get_footer(); ?>
