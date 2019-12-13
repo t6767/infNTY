b6b9497ffde9636de2cfde71adadbb472a82b71c
t6767@mail.ru <t6767@mail.ru>
2019-12-13 15:14:11 +0600

    uy

--- page-resheniya.php
+++ page-resheniya.php
@@ -60,7 +60,7 @@
                                 array(
                                     'post_type' => 'post',
                                     'post_status' => 'publish',
-                                    'posts_per_page' => 7,
+                                    'posts_per_page' => 4,
                                     'cat' => [23],
                                     'post__not_in' => array(get_the_ID()),
                                     'orderby' => 'date',
