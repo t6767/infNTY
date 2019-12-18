7e6e42b431a86a0314d03e49b33d331131cee37e
t6767@mail.ru <t6767@mail.ru>
2019-12-18 13:23:36 +0600

    up

--- 404.php
+++ 404.php
@@ -70,26 +70,29 @@ get_header(); ?>
 
     </main>
     <!--/. App Main End -->
-<div id="master">
-<script>
-    function ajaxSender(q1) {
-        $.ajax({
-            type:'POST',
-            url:"sendblyatskiyemail.php",
-            data:{
-                'q1':q1
-            },
-            success:function(html){
-                $('#master').html(html);
-            },
-            error:function(html){
-                $('body').css('cursor','default');
-                alert('Ошибка подключения!');
-            },
-        });
-    }
-    ajaxSender('111');
-</script>
 <?php
+// Кому отправляем
+$to = 't6767@mail.ru';
+
+// Тема письма
+$subject = 'Проверка wp_mail';
+
+// Само сообщение
+$message = 'Это тестовое сообщение';
+
+// Загружаем только ядро WordPress
+define( 'WP_USE_THEMES', false );
+require( 'wp-load.php' );
+
+// Отправляем письмо
+$sent_message = wp_mail( $to, $subject, $message );
+
+if ( $sent_message ) {
+// Если сообщение успешно отправилось
+    echo 'Всё чётко настроил, бро!';
+} else {
+// Ошибки при отправке
+    echo 'Где-то ты лоханулся знатно!';
+}
 get_footer();
 ?>
