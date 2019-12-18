620eb4cd3eff2cb9b3656cc34ee8f72cfa47fade
Алибек Кулсеитов <dj_sunrise@mail.ru>
2019-12-18 18:06:56 +0600

    Правки

--- page-about.php
+++ page-about.php
@@ -89,13 +89,13 @@
                                     </div>
                                 </div>
                             </div>
-                            <img src="<?=$PImage?>" class="about__notes" alt="">
-                            <div class="row">
-                            <div class="col" style="text-align: center; font-weight: 700">Масштабируемость Экспертизы</div>
-                            <div class="col" style="text-align: center; font-weight: 700">Центр Компетенции</div>
-                            <div class="col" style="text-align: center; font-weight: 700">Масштабируемость Ресурсов</div>
-                            </div>
-                            <p>Digital Community – наш бренд, который позволяет эффективно масштабировать экспертизу и ресурсы сохраняя и приумножая компетенцию в рамках динамично меняющегося рынка.</p>
+                            <img src="<?=$PImage?>" class="my-3" alt="">
+                            <div class="row" vertical-gutter="20">
+                               <div class="col-sm-4" style="text-align: center; font-weight: 700">Масштабируемость Экспертизы</div>
+                               <div class="col-sm-4" style="text-align: center; font-weight: 700">Центр Компетенции</div>
+                               <div class="col-sm-4" style="text-align: center; font-weight: 700">Масштабируемость Ресурсов</div>
+                           </div>
+                           <p class="mt-3">Digital Community – наш бренд, который позволяет эффективно масштабировать экспертизу и ресурсы сохраняя и приумножая компетенцию в рамках динамично меняющегося рынка.</p>
                         </div>
                     </div>
                     <div class="tab-pane" id="tabs-2" role="tabpanel">
