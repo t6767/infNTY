3973528ed1db0512ab79db892fff3ed99b7bd108
t6767@mail.ru <t6767@mail.ru>
2019-12-18 13:08:32 +0600

    Отправка письма вариант 3

--- functions.php
+++ functions.php
@@ -29,4 +29,21 @@ function new_excerpt_more($more) {
     return " <a class='intro-news__link' href='". get_permalink($post->ID) . "'>Подробнее</a>";
 
 }
+/**
+ * Настройка SMTP
+ *
+ * @param PHPMailer $phpmailer объект мэилера
+ */
+function mihdan_send_smtp_email( PHPMailer $phpmailer ) {
+    $phpmailer->isSMTP();
+    $phpmailer->Host       = SMTP_HOST;
+    $phpmailer->SMTPAuth   = SMTP_AUTH;
+    $phpmailer->Port       = SMTP_PORT;
+    $phpmailer->Username   = SMTP_USER;
+    $phpmailer->Password   = SMTP_PASS;
+    $phpmailer->SMTPSecure = SMTP_SECURE;
+    $phpmailer->From       = SMTP_FROM;
+    $phpmailer->FromName   = SMTP_NAME;
+}
+add_action( 'phpmailer_init', 'mihdan_send_smtp_email' );
 
