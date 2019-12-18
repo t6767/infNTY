badf199257a9c0e8821d2fc5103d335f46421bac
t6767@mail.ru <t6767@mail.ru>
2019-12-18 13:26:59 +0600

    up

--- 404.php
+++ 404.php
@@ -71,28 +71,5 @@ get_header(); ?>
     </main>
     <!--/. App Main End -->
 <?php
-// Кому отправляем
-$to = 't6767@mail.ru';
-
-// Тема письма
-$subject = 'Проверка wp_mail';
-
-// Само сообщение
-$message = 'Это тестовое сообщение';
-
-// Загружаем только ядро WordPress
-define( 'WP_USE_THEMES', false );
-require( 'wp-load.php' );
-
-// Отправляем письмо
-$sent_message = wp_mail( $to, $subject, $message );
-
-if ( $sent_message ) {
-// Если сообщение успешно отправилось
-    echo 'Всё чётко настроил, бро!';
-} else {
-// Ошибки при отправке
-    echo 'Где-то ты лоханулся знатно!';
-}
 get_footer();
 ?>
