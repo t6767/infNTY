05754fa723d639f5e875b9b638aadf33a4345be1
t6767@mail.ru <t6767@mail.ru>
2019-12-19 15:03:34 +0600

    up suka poluchay konec

--- category.php
+++ category.php
@@ -78,9 +78,8 @@
                             }
                             ?>
                         </div>
-                        <button id="bider" onclick="getAjaxXXX(10, '<?=$category_id?>', ids)">Показать еще</button>
-                        <div class="text-center mt-5">
-                            <a href="javascript:void(0);" id="linkesho" onclick="document.getElementById('linkesho').style.display='none'; document.getElementById('showmore').style.display='flex'; ">Посмотреть ещё</a>
+                        <div class="text-center mt-5" id="bider">
+                            <a href="javascript:void(0);" onclick="getAjaxXXX(10, '<?=$category_id?>', ids)">Посмотреть ещё</a>
                         </div>
                     </section>
                 </div>
