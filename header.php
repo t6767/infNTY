39dc44a63f7424a5ff1a95146f7035e11ce3c17a
t6767@mail.ru <t6767@mail.ru>
2019-12-20 17:17:11 +0600

    up

--- header.php
+++ header.php
@@ -125,7 +125,10 @@ function the_excerpt_max_charlength( $charlength ){
 </head>
 
 <body style="background: #f9f9f9">
-
+<?php
+$fblnk = get_post_meta( 566, 'fblink' );
+$lklnk = get_post_meta( 566, 'linkedinlink' );
+?>
 <div class="menu">
     <div class="menu__col">
         <button type="button" class="menu__close">Закрыть</button>
@@ -148,8 +151,8 @@ function the_excerpt_max_charlength( $charlength ){
                     <a href="tel:+77272620826" class="menu__phone">+7 (727) 262 08 26</a>
                 </div>
                 <div class="menu__social">
-                    <a href="#" class="menu__footer-link -facebook"></a>
-                    <a href="#" class="menu__footer-link -linkedin"></a>
+                    <a href="<?=$fblnk?>" class="menu__footer-link -facebook"></a>
+                    <a href="<?=$lklnk?>" class="menu__footer-link -linkedin"></a>
                 </div>
             </div>
         </div>
