260596093cc201a7ca0b35fb0b6b67dae894f340
t6767@mail.ru <t6767@mail.ru>
2019-12-24 00:13:09 +0600

    up

--- header.php
+++ header.php
@@ -24,7 +24,7 @@ function the_excerpt_max_charlength( $charlength ){
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title><?php wp_title(''); ?></title>
+    <title><?php wp_title(''); ?> - 123</title>
     <meta content="<?php bloginfo('description'); ?>" name="description">
     <meta content="" name="keywords">
     <meta name="viewport" content="width=device-width, initial-scale=1">
