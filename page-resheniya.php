3d663b47e5fe96f7d24f0be70ba6ca02a97b189d
Алибек Кулсеитов <dj_sunrise@mail.ru>
2019-12-23 17:10:34 +0600

    Исправил страницу решения

--- page-resheniya.php
+++ page-resheniya.php
@@ -32,7 +32,7 @@
         <div class="page__content">
         <div class="container">
             <div class="page__topbar">
-                <button type="button" class="button -bordered" data-target="#modal-1" data-toggle="modal">связаться с нами</button>
+                <button type="button" class="button -bordered ml-auto" data-target="#modal-1" data-toggle="modal">связаться с нами</button>
             </div>
             <div class="row">
                 <div class="col">
@@ -78,52 +78,48 @@
                             </div>
                         <?php }}  ?>
                 </div>
+                        <form class="form contacts__form mb-5">
+                            <h2 class="form__title">Написать нам</h2>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="control">
+                                        <input type="text" id="resheniyaname" onblur="proverkaResheniya()" oninput="proverkaResheniya()"  name="name" class="control__input" placeholder="Имя">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="control">
+                                        <input type="text" id="resheniyaphone"  onblur="proverkaResheniya()" oninput="proverkaResheniya()" name="phone" class="control__input" placeholder="+7 (***) ***-**-**">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="control">
+                                        <input type="text" id="resheniyaemail"  onblur="proverkaResheniya()" oninput="proverkaResheniya()"  name="email" class="control__input" placeholder="Email">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="control mt-md-3">
+                                <textarea onblur="proverkaResheniya()" oninput="proverkaResheniya()" id="resheniyamessage" class="control__textarea" placeholder="Сообщение"></textarea>
+                            </div>
+                            <div class="text-center">
+                                <button id="buttonsendreshen" type="button" onclick="getAjaxContactForm('2', document.getElementById('resheniyaname').value, document.getElementById('resheniyamessage').value, document.getElementById('resheniyaphone').value, document.getElementById('resheniyaemail').value);" data-target="#modal-3" data-toggle="modal" class="button -bordered form__button" disabled>отправить</button>
+                            </div>
+                        </form>
+                        <script>
+                            function proverkaResheniya() {
+                                nameResh=nm(document.getElementById('resheniyaname').value);
+                                messResh=nm(document.getElementById('resheniyamessage').value);
+                                telResh=ph(document.getElementById('resheniyaphone').value);
+                                emailResh=ff(document.getElementById('resheniyaemail').value);
+                                if(emailResh==1 && telResh==1 && nameResh==1 && messResh==1) document.getElementById('buttonsendreshen').disabled = false; else document.getElementById('buttonsendreshen').disabled = true;
+                            }
+                            var emailResh=0;
+                            var telResh=0;
+                            var nameResh=0;
+                            var messResh=0;
+                        </script>
             </div>
         </div>
         <!--/. Page Content End -->
-        <div class="container">
-            <div class="row">
-                <div class="col">
-        <form class="form contacts__form">
-            <h2 class="form__title">Написать нам</h2>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="control">
-                        <input type="text" id="resheniyaname" onblur="proverkaResheniya()" oninput="proverkaResheniya()"  name="name" class="control__input" placeholder="Имя">
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="control">
-                        <input type="text" id="resheniyaphone"  onblur="proverkaResheniya()" oninput="proverkaResheniya()" name="phone" class="control__input" placeholder="+7 (***) ***-**-**">
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="control">
-                        <input type="text" id="resheniyaemail"  onblur="proverkaResheniya()" oninput="proverkaResheniya()"  name="email" class="control__input" placeholder="Email">
-                    </div>
-                </div>
-            </div>
-            <div class="control mt-md-3">
-                <textarea onblur="proverkaResheniya()" oninput="proverkaResheniya()" id="resheniyamessage" class="control__textarea" placeholder="Сообщение"></textarea>
-            </div>
-            <div class="text-center">
-                <button id="buttonsendreshen" type="button" onclick="getAjaxContactForm('2', document.getElementById('resheniyaname').value, document.getElementById('resheniyamessage').value, document.getElementById('resheniyaphone').value, document.getElementById('resheniyaemail').value);" data-target="#modal-3" data-toggle="modal" class="button -bordered form__button" disabled>отправить</button>
-            </div>
-        </form>
-        <script>
-            function proverkaResheniya() {
-                nameResh=nm(document.getElementById('resheniyaname').value);
-                messResh=nm(document.getElementById('resheniyamessage').value);
-                telResh=ph(document.getElementById('resheniyaphone').value);
-                emailResh=ff(document.getElementById('resheniyaemail').value);
-                if(emailResh==1 && telResh==1 && nameResh==1 && messResh==1) document.getElementById('buttonsendreshen').disabled = false; else document.getElementById('buttonsendreshen').disabled = true;
-            }
-            var emailResh=0;
-            var telResh=0;
-            var nameResh=0;
-            var messResh=0;
-        </script>
-                </div></div></div>
     </div>
     <!--/. Page End -->
 
