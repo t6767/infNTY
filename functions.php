ee70ec0a68e2023b4aa4d852362955ba1e767845
t6767@mail.ru <t6767@mail.ru>
2019-12-26 16:32:19 +0600

    up

--- functions.php
+++ functions.php
@@ -63,7 +63,7 @@ function advanced_search_query($query) {
 
         // tag search
         if (isset($_GET['tag'])) {
-            $query->set('tag', 'qr');
+        //    $query->set('tag', 'qr');
         }
         return $query;
     }
