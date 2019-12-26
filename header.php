7f083fa7b8b2d6000402a37a7fb121db745dadea
t6767@mail.ru <t6767@mail.ru>
2019-12-26 16:09:32 +0600

    gf

--- header.php
+++ header.php
@@ -137,9 +137,6 @@ $lklnk = get_post_meta( 566, 'linkedinlink' )[0];
             <form class="menu__search" id="searchform" method="get" action="<?php echo home_url('/'); ?>">
                 <input type="search" class="menu__search-input" name="s" value="<?php the_search_query(); ?>">
                 <input type="hidden" name="category_name" value="События" />
-                <input type="hidden" name="post_type[]" value="magazine" />
-                <input type="hidden" name="post_type[]" value="ebook" />
-                <input type="hidden" name="post_type[]" value="pdf" />
                 <button type="submit" class="menu__search-button" value="Search">Искать</button>
             </form>
             <?php
